d25fed51c4b2db12b94c6b9edf5707bb2b1475e3
Daniel Mora <daniel_k310a@hotmail.com>
2024-11-06 13:44:54 -0600

    feat: update dependencies and app

--- projects/basic-concepts/src/App.tsx
+++ projects/basic-concepts/src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css'
+import Avatar from './components/AvatarGit'
 
 function App() {
   return (
     <div className="flex justify-center items-center bg-gray-100 h-screen">
-      <div className="text-center">
+      <div className="flex flex-wrap justify-center gap-4 w-full text-center">
         <h3> Bienvenidos al curso express </h3>
         <h2>
           <strong>React + Typescript + Tailwind + todo lo que quieran</strong>{' '}
         </h2>
+        <div className="flex flex-wrap justify-center items-center gap-4 w-1/2">
+          <Avatar name="Daniel el bello" github="danimc" />
+          <Avatar name="El Bollito Dev" github="bollitoDev" />
+          <Avatar name="Abraham El poderoso" github="alegorreta-mex" />
+          <Avatar name="Santi el Zen" github="prsantiago" />
+        </div>
       </div>
     </div>
   )
