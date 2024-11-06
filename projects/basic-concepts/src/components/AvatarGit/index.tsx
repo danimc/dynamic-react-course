02881bdc1b61cacc998fbfe4e53a14e2d9c75c48
Daniel Mora <daniel_k310a@hotmail.com>
2024-11-06 13:28:02 -0600

    feat: add AvatarGit component

---
+++ projects/basic-concepts/src/components/AvatarGit/index.tsx
@@ -0,0 +1,8 @@
+export default function AvatarGit() {
+  return (
+    <div className="flex justify-start items-center gap-4 bg-blue-900 hover:bg-blue-700 p-4 rounded-lg text-white hover:cursor-pointer">
+      <img src="https://unavatar.io/danimc" className="rounded-full w-14 h-14" />
+      <h1>Yo soy Dani</h1>
+    </div>
+  )
+}
