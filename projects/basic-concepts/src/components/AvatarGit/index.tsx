f12b16986fcdfc23d1b701297e7517a1fc6d090b
Daniel Mora <daniel_k310a@hotmail.com>
2024-11-06 13:44:34 -0600

    feat: update AvatarGit component to accept name and github props

--- projects/basic-concepts/src/components/AvatarGit/index.tsx
+++ projects/basic-concepts/src/components/AvatarGit/index.tsx
@@ -1,8 +1,8 @@
-export default function AvatarGit() {
+export default function AvatarGit({ name, github }) {
   return (
-    <div className="flex justify-start items-center gap-4 bg-blue-900 hover:bg-blue-700 p-4 rounded-lg text-white hover:cursor-pointer">
-      <img src="https://unavatar.io/danimc" className="rounded-full w-14 h-14" />
-      <h1>Yo soy Dani</h1>
+    <div className="flex justify-start items-center gap-4 bg-blue-900 hover:bg-blue-700 p-4 rounded-lg w-full text-white hover:cursor-pointer">
+      <img src={`https://unavatar.io/${github}`} className="rounded-full w-14 h-14" />
+      <h1>Yo soy {name}</h1>
     </div>
   )
 }
